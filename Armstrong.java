fb2278965acf4e1bc375a99abbe88048f0c15aae
Meghana-TechThinker <chowdarymeghana17@gmail.com>
2025-03-21 18:22:53 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,18 +1,28 @@
-class Count{
-    public static void main(String[] args){
-        int count=0;
-        int n=153;
-        int t=n;
-        while (n!=0){
-            int r=n%10;
-            count++;
-            n=n/10;
+import java.util.*;
+public class Empty {
+        public static void main(String[] args){
+            int count=0;
+            int n=153;
+            int t=n;
+            while (n!=0){
+                int r=n%10;
+                count++;
+                n=n/10;
+            }
+            System.out.println(count);
+            int s=0,rem;
+            n=t;
+            while (n!=0){
+                rem=n%10;
+                s=s+(int)Math.pow(rem,count);
+                n=n/10;  
+            }
+            System.out.print(s);
+            if(s==t){
+                System.out.println("Armstrong");
+            }
+            else{
+                System.out.println(" Not Armstrong");
+            }
         }
-        System.out.println(count);
-        while (t!=0){
-            int rem=t%10;
-            rem=rem**count;
-            
-        }
-    }
 }
